ff8a143840ca7af4d035bfd0b6e57d646a9f79c4
Mikael Jorda <mikael.jorda@gmail.com>
2017-05-14 20:29:05 -0700

    added a few halper functions plus comments

--- src/keripiav_helper_functions.py
+++ src/keripiav_helper_functions.py
@@ -1,12 +1,17 @@
 import numpy as np
 import cv2
 
+# retuens true if the line is globally vertical (slope more than 1)
 def isVerticalLine(x1,y1,x2,y2):
 	if(x1==x2 and y1==y2):
 		print "\nWARNING : not a line, the two points are the same\n\n"
 		return
 	return(abs(y2-y1) > abs(x2-x1))
 
+def isVerticalLineArray(l):
+	return isVerticalLine(l[0],l[1],l[2],l[3])
+
+# compute the line formed by the two leftmost points of the ends of the two lines given as arguments
 def resultingVerticalLeftLine(l1,l2):
 	# makes a left line out of the two left most points
 	if(not (isVerticalLine(l1[0],l1[1],l1[2],l1[3]) and isVerticalLine(l2[0],l2[1],l2[2],l2[3]))):
@@ -59,6 +64,7 @@ def resultingVerticalLeftLine(l1,l2):
 
 	return [int(resulting_top[0]),int(resulting_top[1]),int(resulting_bottom[0]),int(resulting_bottom[1])]
 
+# compute the line formed by the two rightmost points of the ends of the two lines given as arguments
 def resultingVerticalRightLine(l1,l2):
 	# makes a left line out of the two left most points
 	if(not (isVerticalLine(l1[0],l1[1],l1[2],l1[3]) and isVerticalLine(l2[0],l2[1],l2[2],l2[3]))):
@@ -111,6 +117,7 @@ def resultingVerticalRightLine(l1,l2):
 
 	return [int(resulting_top[0]),int(resulting_top[1]),int(resulting_bottom[0]),int(resulting_bottom[1])]
 
+# compute the line formed by the two bottommost points of the ends of the two lines given as arguments
 def resultingHorizontalBottomLine(l1,l2):
 	# makes a left line out of the two left most points
 	if(isVerticalLine(l1[0],l1[1],l1[2],l1[3]) or isVerticalLine(l2[0],l2[1],l2[2],l2[3])):
@@ -163,12 +170,18 @@ def resultingHorizontalBottomLine(l1,l2):
 
 	return [int(resulting_left[0]),int(resulting_left[1]),int(resulting_right[0]),int(resulting_right[1])]
 
+# return the line equation 
 def lineEquation(x1,y1,x2,y2):
 	if(isVerticalLine(x1,y1,x2,y2)):
 		return verticalLineEquation(x1,y1,x2,y2)
 	else:
 		return horizontalLineEquation(x1,y1,x2,y2)
 
+# return the line equation when the inpus is an array such that l = [x1,y1,x2,y2] 
+def lineEquationArray(l):
+	return lineEquation(l[0],l[1],l[2],l[3])
+
+# return the equation of a vertical line
 def verticalLineEquation(x1,y1,x2,y2):
 	a = 1.
 	b = -(float(x1)-float(x2))/(float(y1)-float(y2))
@@ -176,6 +189,7 @@ def verticalLineEquation(x1,y1,x2,y2):
 
 	return np.array([a,b,c])
 
+# return the equation of a horizontal line
 def horizontalLineEquation(x1,y1,x2,y2):
 	a = -(float(y1)-float(y2))/(float(x1)-float(x2))
 	b = 1.
@@ -183,6 +197,7 @@ def horizontalLineEquation(x1,y1,x2,y2):
 
 	return np.array([a,b,c])
 
+# computes the y coordinate corresponding to the given line and x
 def computeYOnLine(line_equation, x):
 	a = line_equation[0]
 	b = line_equation[1]
@@ -192,6 +207,7 @@ def computeYOnLine(line_equation, x):
 		return
 	return (float) (-c-a*x)/b
 
+# computes the x coordinate corresponding to the given line and y
 def computeXOnLine(line_equation, y):
 	a = line_equation[0]
 	b = line_equation[1]
@@ -200,3 +216,72 @@ def computeXOnLine(line_equation, y):
 		print "\nWARNING : horizontal line, impossible to compute x in computeXOnLine\n\n"
 		return
 	return (float) (-c-b*y)/a
+
+# finds the polygon to the left of the vertical line given as argument in the image
+def findLeftPolygon(line, img_height, img_width):
+	if(!isVerticalLine(line)):
+		print "\nWARNING : line not vertical in findLeftPolygon\n\n"
+	eq = lineEquationArray(line)
+	xtop = computeXOnLine(eq,0)
+	xbottom = computeXOnLine(eq, img_height)
+	p1 = np.array([xtop,0], np.int32)
+	p2 = np.array([xbottom,img_height], np.int32)
+	if(xtop > 0):
+		p0 = np.array([0,0], np.int32)
+		if(xbottom > 0):
+			p3 = np.array([0,img_height], np.int32)
+			return np.array([p0,p1,p2,p3])
+		return np.array([p0,p1,p2])
+	else:
+		if(xbottom > 0):
+			p3 = np.array([0,img_height])
+			return np.array([p1,p2,p3]) 
+		else:
+			print "\nWARNING : should not reach here in findLeftPolygon\n\n"
+
+# finds the polygon to the right of the vertical line given as argument in the image
+def findRightPolygon(line, img_height, img_width):
+	if(!isVerticalLine(line)):
+		print "\nWARNING : line not vertical in findRightPolygon\n\n"
+	eq = lineEquationArray(line)
+	xtop = computeXOnLine(eq,0)
+	xbottom = computeXOnLine(eq, img_height)
+	p1 = np.array([xtop,0], np.int32)
+	p2 = np.array([xbottom,img_height], np.int32)
+	if(xtop < img_width):
+		p0 = np.array([img_width,0], np.int32)
+		if(xbottom < img_width):
+			p3 = np.array([img_width,img_height], np.int32)
+			return np.array([p0,p1,p2,p3])
+		return np.array([p0,p1,p2])
+	else:
+		if(xbottom < img_width):
+			p3 = np.array([img_width,img_height])
+			return np.array([p1,p2,p3]) 
+		else:
+			print "\nWARNING : should not reach here in findRightPolygon\n\n"
+
+def findBottomPolygon(line, img_height, img_width):
+	if(isVerticalLine(line)):
+		print "\nWARNING : line not horizontal in findBottomPolygon\n\n"
+	eq = lineEquationArray(line)
+	yleft = computeYOnLine(eq,0)
+	yright = computeYOnLine(eq, img_width)
+	p1 = np.array([0,yleft], np.int32)
+	p2 = np.array([img_width, yright], np.int32)
+	if(yleft < img_height):
+		p0 = np.array([0,img_height], np.int32)
+		if(yright < img_height):
+			p3 = np.array([img_width,img_height], np.int32)
+			return np.array([p0,p1,p2,p3])
+		return np.array([p0,p1,p2])
+	else:
+		if(yright < img_height):
+			p3 = np.array([img_width,img_height])
+			return np.array([p1,p2,p3]) 
+		else:
+			print "\nWARNING : should not reach here in findBottomPolygon\n\n"
+
+
+def findCorners(img):
+
